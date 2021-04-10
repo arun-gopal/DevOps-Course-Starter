3c2c99db1f0f125204043bfb34103bb5c2727520
arun-gopal <arungpl@yahoo.com>
2021-04-11 00:22:24 +0100

    Added back end changes for item creation

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,6 +9,13 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
+    return render_template('index.html', items=get_items())
+
+
+@app.route('/items', methods=['POST'])
+def create():
+    title = request.form.get('title')
+    add_item(title)
     return render_template('index.html', items=get_items())
 
 
