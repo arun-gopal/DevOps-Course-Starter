b166ef0ab8091a002365901eec1c633826370ebc
arun-gopal <arungpl@yahoo.com>
2021-05-29 22:31:02 +0100

    Added minimal test

--- todo_app/trello_client.py
+++ todo_app/trello_client.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import json
+from datetime import datetime
+
 
 TRELLO_API_KEY = os.getenv('TRELLO_API_KEY')
 TRELLO_API_TOKEN = os.getenv('TRELLO_API_TOKEN')
@@ -21,7 +23,7 @@ def get_cards_from_list(list_id):
     response.raise_for_status()
     items = []
     for item in response.json():
-        items.append(Item(item["id"], item["name"], item["desc"]))
+        items.append(Item(item["id"], item["name"], item["desc"], item["dateLastActivity"]))
     return items
 
 def create_card_on_todo_list(card_name, description=None):
@@ -32,7 +34,7 @@ def create_card(list_id, card_name, description=None):
     query = {"name": card_name, "desc": description,"idList": list_id, "key": TRELLO_API_KEY, "token": TRELLO_API_TOKEN}
     response = requests.request("POST", url, params=query)
     response.raise_for_status()
-    return Item(response.json()["id"], response.json()["name"], response.json()["desc"])
+    return Item(response.json()["id"], response.json()["name"], response.json()["desc"], response.json()["dateLastActivity"])
 
 def move_card_to_done_list(id):
     return move_card_to_list(DONE_LIST_ID, id)
@@ -46,11 +48,12 @@ def move_card_to_list(list_id, id):
     query = {"key": TRELLO_API_KEY, "token": TRELLO_API_TOKEN, "idList":list_id}
     response = requests.request("PUT", url, headers=headers, params=query)
     response.raise_for_status()
-    return Item(response.json()["id"], response.json()["name"], response.json()["desc"])
+    return Item(response.json()["id"], response.json()["name"], response.json()["desc"], response.json()["dateLastActivity"])
 
 class Item:
 
-  def __init__(self, identity, name, desc):
+  def __init__(self, identity, name, desc, last_modified):
     self.identity = identity  
     self.name = name
     self.desc = desc
+    self.last_modified = datetime.strptime(last_modified, "%Y-%m-%dT%H:%M:%S.%fZ").date()
