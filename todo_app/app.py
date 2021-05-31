176a44d0193e7f215cf091c56f9afe8bd392b627
arun-gopal <arungpl@yahoo.com>
2021-05-31 18:36:52 +0100

    Removed unwanted files

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, redirect
 
-from todo_app.flask_config import Config
 from todo_app.trello_client import get_cards_from_all_lists, create_card_on_todo_list, move_card_to_todo_list, move_card_to_done_list
 from todo_app.trello_client import ItemViewModel
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
 @app.route('/')
 def index():
